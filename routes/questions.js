3cd76dbe2fd6658a9084498a2885bef6040d1414
Serena Leung <serenaleung1@gmail.com>
2017-04-26 13:54:43 -0700

    Add Questions#new and Questions#create

--- routes/questions.js
+++ routes/questions.js
@@ -7,7 +7,7 @@ const Question = require('../models/index').Question;
 
 router.get('/', function (request, response, next){
   Question
-    .findAll()   //resolving the questions so variable is questions
+    .findAll({order: [['createdAt', 'DESC'], ['updatedAt', 'DESC']]})   //resolving the questions so variable is questions
     .then(function(questions) { // allowing us to use .then
       // the path of the template that response.render takes
       // is relative to the view/ folder by default
@@ -18,6 +18,29 @@ router.get('/', function (request, response, next){
     })
   // response.send('Stuff');
   // All Sequelize query methods return a promise
+})
+
+// Questions#new URL: /questions/new VERB: GET
+router.get('/new', function (req, res) {
+  const question = Question.build();
+
+  res.render('questions/new', {question: question});
+})
+
+router.post('/', function (req, res) {
+  // .body is a property of the request object that
+  // contains all form data as a JavaScript object
+  // res.send(req.body);
+  // const title = req.body.title;
+  // const description = req.body.description;
+
+ const {title, description} = req.body;
+
+ Question
+ .create({title: title, description: description})
+ .then(function (question) {
+   res.redirect('/questions');
+ })
 })
 
 router.get('/:id', function (req, res) {
